f8026eb643f9507a728bd39f23d385e1280deb3f
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-24 14:27:39 -0300

    Exercicio 1 finalizado

--- src/Dog.js
+++ src/Dog.js
@@ -1,8 +1,58 @@
 import React from 'react';
 
 class Dog extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foto: '',
+      carregou: false,
+    };
+
+    this.chamarApi = this.chamarApi.bind(this);
+    this.chamarNovoDog = this.chamarNovoDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.chamarApi();
+  }
+
+  async chamarApi() {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const api = await fetch(url);
+    const resultado = await api.json();
+    if (resultado.status === 'success') {
+      const fotoAnimal = resultado.message;
+      this.setState({
+        foto: fotoAnimal,
+        carregou: true,
+      });
+    }
+  }
+
+  chamarNovoDog() {
+    this.setState({
+      carregou: false,
+    });
+    const tempo = 200;
+    setTimeout(() => {
+      this.chamarApi();
+    }, tempo);
+  }
+
   render() {
-    return ();
+    const { foto, carregou } = this.state;
+    if (carregou === false) {
+      return (
+        <p>Loading...</p>
+      );
+    }
+    return (
+      <div>
+        <img src={ foto } alt="foto de animal" />
+        <button type="submit" onClick={ this.chamarNovoDog }>Novo Dog</button>
+      </div>
+    );
   }
 }
 
